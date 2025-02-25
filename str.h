9ca04c5e77672223140628b19cf73bc65f40d0d1
Ziya Momin <ziyamomin@gmail.com>
2025-02-25 18:08:50 -0500

    Fixed comments

--- str.h
+++ str.h
@@ -5,19 +5,27 @@
 #include <stdlib.h>
 
 
-/* This function computes the length of the passed in string parameter s. The function attempts to compute the length of s and returns the number of characters, not including the trailing null character.*/
+/* This function computes the length of the passed in string parameter pcSrc. The function attempts to compute the length of pcSrc and returns the number of characters, not including the trailing null character.*/
 size_t Str_getLength(const char *pcSrc);
 
-/* This function copies the characters of the array pointed to by parameter s2 into the array pointed to by parameter s1. If it encounters a null character in the array pointed to by s2, the function adds null characters to the array pointed by s1 until a total of n characters have been written. s1, a pointer to the destination, is returned.*/
+/* This function copies the characters from the string pointed to by pcSrc into the array 
+   pointed to by pcDest. It copies characters until it encounters the null character in pcSrc, 
+   then null-terminates pcDest. The string pcDest must have sufficient space to hold the result. 
+   Returns a pointer to the destination, pcDest. */
 char *Str_copy(char *pcDest, const char *pcSrc);
 
-/* This function appeneds a copy of the null-terminated string s2 parameter to the end of the null-terminated string s1 parameter, then add a terminating ‘\0’.  The string s1 must have sufficient space to hold the result. The pointer s1 is returned. */
+/* This function appends a copy of the null-terminated string pcSrc parameter to the end of the null-terminated string pcDest parameter, then adds a terminating ‘\0’.  The string pcDest must have sufficient space to hold the result. The pointer pcDest is returned. */
 char *Str_concat(char *pcDest, const char *pcSrc);
 
-/* This function lexigraphically compares the passed in strings s1 and s2. A negative integer is returned if s1 is lexicographically less than s2. Zero is returned if s1 is lexicographically equal to s2. A positive integer is returned if s1 is lexicographically greater than s2. */
+/* This function lexicographically compares the two parameter strings pcSrc1 and pcSrc2. 
+   It returns a negative value if pcSrc1 is less than pcSrc2, 
+   zero if they are equal, and a positive value if pcSrc1 is greater than pcSrc2. */
 int Str_compare(const char *pcSrc1, const char *pcSrc2);
 
-/* A pointer to the first occurrence in the string pointed to by parameter s1 of the sequence of characters in the string pointed to by parameter s2. Returns a null pointer if no match is found. */
+/* This function searches for the first occurrence of the string parameter pcNeedle 
+   within the string parameter pcHaystack. If found, it returns a pointer to the first matching substring 
+   in pcHaystack. If no match is found, it returns a null pointer. */
 const char *Str_search(const char *pcHaystack, const char *pcNeedle);
 
 #endif
+
