2a1aca517ff58b5ddb482b6929f7c27d09fe57e3
Ziya Momin <ziyamomin@gmail.com>
2025-02-24 19:20:29 -0500

    Fix copy method to get rid of block checking n

--- stra.c
+++ stra.c
@@ -25,9 +25,6 @@ char *Str_copy(char s1[], const char s2[]) {
     for(i = 0; s2[i] != '\0'; i++) {
         s1[i] = s2[i];
     }
-    while(i < n) {
-        s1[i++] = '\0';
-    }
     return s1;
 }
 
